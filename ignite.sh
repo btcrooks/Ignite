e468f7a4428cecc44130637b9c3c649c45cd42e9
Brandon Crooks <crookrb@hotmail.com>
2015-05-12 21:41:25 -0400

    Micro updates
    - Remove tree command
    - Checks for MAMP installation
    - Remove DS_Store bullshit command

--- ignite.sh
+++ ignite.sh
@@ -1,44 +1,48 @@
 #!/bin/sh
-#Author: Brandon Crooks
-#Description: A shell script that sets preferences
-# for VIM, and aliases for BASH and ZSH. 
+#
+# Author: Brandon Crooks
+# Version: 2.1
+# Description: A shell script that sets preferences
+#  for VIM, and aliases for BASH and ZSH. 
 #----------Color lib---------------------
 yellow="\033[0;33m"
 green="\033[0;32m"
 nc="\033[0m"
 #----------------------------------------
+
 # Setup file paths
 Bash_Profile="$HOME/.bash_profile"
 ZSHRC="$HOME/.zshrc"
 VIMRC="$HOME/.vimrc"
 
-# Setup alias variables
-MAMPSERVE='alias mamp-serve="/Applications/MAMP/bin/start.sh" && echo"Starting Server: 127.0.0.1:8888/MAMP/" && open http://127.0.0.1:8888/MAMP/'
-MAMPSTOP='alias mamp-stop="/Applications/MAMP/bin/stop.sh"'
+## Setup alias variables
+# Add MAMP shortcuts only if MAMP exists
+if [[ -d "/Applications/MAMP" ]]; then
+  MAMPSERVE='alias mamp-serve="/Applications/MAMP/bin/start.sh" && echo"Starting Server: 127.0.0.1:8888/MAMP/" && open http://127.0.0.1:8888/MAMP/'
+  MAMPSTOP='alias mamp-stop="/Applications/MAMP/bin/stop.sh"'
+else
+  MAMPSERVE='alias mamp-serve="echo MAMP is not installed."'
+  MAMPSTOP='alias mamp-serve="echo MAMP is not installed."'
+fi
 TESTING='alias marco="echo Polo"'
-KILLDS_STORE='alias killds_store="find . -name .DS_Store -print0 | xargs -0 git rm -f --ignore-unmatch"'
 
-# Add shortcuts to .bash_profile
+## Add shortcuts to .bash_profile
 /bin/cat <<EOF >> $Bash_Profile
 $MAMPSERVE
 $MAMPSTOP
-$TESTING 
-$KILLDS_STORE
-$TREE
+$TESTING
 EOF
 echo "${yellow}bash shortcuts ready..."
  
-# Add shortcuts to .zshrc
+## Add shortcuts to .zshrc
 /bin/cat <<EOF >> $ZSHRC
 $MAMPSERVE
 $MAMPSTOP
-$TESTING 
-$KILLDS_STORE
-$TREE
+$TESTING
 EOF
 echo "${yellow}zsh shortcuts ready..."
  
-# Setup VIM defaults
+## Setup VIM defaults
 /bin/cat <<EOF >> $VIMRC
 syntax on
 set numberwidth=2
@@ -53,10 +57,10 @@ set omnifunc=javascriptcomplete#CompleteJS
 EOF
 echo "${yellow}Vim presets set..."
  
-# Refresh bash
+## Refresh
 sleep 1
 source ~/.bash_profile
 
-# Let us know when your done
+## Let us know when your done & exit
 echo "${green}I'm all done!${nc}"
 exit 0
