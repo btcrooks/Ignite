10e2475f63ee6ab659515ea26f70b2e47196478b
Brandon Crooks <brandoncrooks@Brandons-iMac.local>
2015-12-01 01:00:27 -0500

    Add desc and color lib

--- uninstall.sh
+++ uninstall.sh
@@ -3,10 +3,10 @@
 # @author: Brandon Crooks
 # @version: 1.5
 #
-# Description: Simple bash functions, preferences
-# for VIM, and helpful aliases for BASH and ZSH.
+# @description: Removes ignite settings and reloads
+# previous .bash_profile.
 #
-#
+
 #----------Color lib---------------------
 # yellow="\033[0;33m"
 # green="\033[0;32m"
@@ -15,6 +15,7 @@ green() { printf "\033[32m${1}\033[0m\n"; }
 yellow() { printf "\033[33m${1}\033[0m\n"; }
 red() { printf "\033[31m${1}\033[0m\n"; }
 #----------------------------------------
+
 echo "Uninstalling '.bash_profile'"
 rm ~/.bash_profile
 echo "Uninstalling '.zshrc'"
